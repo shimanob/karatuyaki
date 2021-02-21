415c365da95664926d3a3bd356e8c645122fbd16
nobuhiko shimaya <shimayanobuhiko@gmail.com>
2021-02-22 00:40:28 +0900

    images変更

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Item(models.Model):
     category = models.CharField(max_length=100)
     slug = models.SlugField()
     description = models.TextField()
-    image = models.ImageField(upload_to='image')
+    image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.title
